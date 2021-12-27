287a2c0a0327a37d7a575cbb78bff2502941572f
dan <danilsocol-2002@mail.ru>
2021-12-28 01:24:12 +0500

    12

---
+++ test5/MyDictionary.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace test5
+{
+    class MyDictionary<T>
+    {
+        Cell<int, T>[] arr;
+        public readonly uint size;
+        int j;
+        public int maxJ = 0;
+        public MyDictionary(uint size)
+        {
+            arr = new Cell<int, T>[size];
+            this.size = size;
+        }
+
+        public void Add(T value)
+        {
+            j = 1;
+            int key = GetHashCode(value);
+            while (true)
+            {
+                if (key > size)
+                    key -= 10000;
+
+                if (maxJ < j)
+                    maxJ = j;
+
+                try
+                {
+                    if (arr[key] != null)
+                        throw new Exception("This key is already contained in the dictionary");
+
+                    arr[key] = new Cell<int, T>(key, value);
+                    Console.WriteLine(key);
+                    return;
+                }
+                catch
+                {
+                    key = FindVoidCell(key);
+                    j++;
+                }
+            }
+           
+        }
+        
+        public int FindVoidCell(int numCell)
+        {
+            Console.WriteLine("изменение ячейки");
+
+            Console.WriteLine($"{numCell} + {(int)Math.Pow(j, 2)} = {numCell + (int)Math.Pow(j, 2)}");
+            return numCell += (int)Math.Pow(j, 2);
+        }
+
+        public void Remove(int key)
+        {
+            Cell<int, T> cell = Search(key);
+            cell = null;
+        }
+
+        public Cell<int,T> Search(int key)
+        {
+            foreach (var item in arr)
+            {
+                if (item.key.Equals(key))
+                    return item;
+            }
+            throw new IndexOutOfRangeException("Incorrect Key");
+        }
+
+        private  int GetHashCode(T key)
+        {
+            double value = key is int key2 ? 0.59756416 * key2 : 0.59756416 * key.ToString()[0];
+            var trunc = Math.Truncate(value);
+            var hash = (int)(size * (value - trunc));
+            return hash;
+        }
+    }
+        internal class Cell<TKey, TValue>
+        {
+            internal TValue value;
+            internal TKey key;
+            internal Cell(TKey key, TValue value)
+            {
+                this.value = value;
+                this.key = key;
+            }
+        }
+}
+
---
+++ test5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.IO;
+
+namespace test5
+{
+    using System;
+    using System.Collections.Generic;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int> dictionary = new MyDictionary<int>(10000);
+            Random rnd = new Random();
+
+            for (int i = 0; i < 9000; i++)
+            {
+                int num = rnd.Next();
+                dictionary.Add( num);
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"max {dictionary.maxJ}");
+        }
+
+       
+       
+    }
+}
+
+
